dbfcbff6b8254427ed7c727ae20e35777f0fe56d
Kira Zaraki <kira-schiffer@hotmail.fr>
2026-03-30 19:13:09 +0100

    add ressources logique

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -6,28 +6,29 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Site;
+use App\Http\Resources\ProductResource;
 
 class ProductController extends Controller
 {
     public function productListe()
     {
-        return $this->success(Product::with('prices')->paginate(10), 'Produits récupérés avec succès');
+        return $this->success(ProductResource::collection(Product::with('prices')->paginate(10)), 'Produits récupérés avec succès');
     }
 
     public function show(Product $product)
     {
-        return $this->success($product->load('prices'), 'Produit récupéré avec succès');
+        return $this->success(new ProductResource($product), 'Produit récupéré avec succès');
     }
 
     public function productsBySite($site)
     {   
         $products = Product::bySite($site)->get();
 
-        return $this->success($products, 'Produits récupérés avec succès');
+        return $this->success(new ProductResource($product), 'Produits récupérés avec succès');
     }
 
     public function showBySite(Site $site, Product $product)
     {
-        return $this->success($product, 'Produit récupéré avec succès');
+        return $this->success(new ProductResource($product), 'Produit récupéré avec succès');
     }
 }
